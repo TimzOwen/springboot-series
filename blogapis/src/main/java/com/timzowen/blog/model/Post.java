40d470787ca46eb593bbbd7c5df7c393d71ba6b4
TimzOwen <timzowen@gmail.com>
2024-02-19 22:42:07 +0300

    entity set up

--- blogapis/src/main/java/com/timzowen/blog/model/Post.java
+++ blogapis/src/main/java/com/timzowen/blog/model/Post.java
@@ -1,8 +1,25 @@
 package com.timzowen.blog.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "posts",  uniqueConstraints = {@UniqueConstraint(columnNames = "title")})
 public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String description;
+    @Column(name = "title", nullable = false)
     private String title;
+    @Column(name = "description", nullable = false)
+    private String description;
+    @Column(name = "contents",nullable = false )
     private String content;
 }
