1e7f776f10c8f918753bbcc05c86ee97ab1a5897
TimzOwen <timzowen@gmail.com>
2024-02-20 22:59:45 +0300

    updared Get all end point

--- blogapis/src/main/java/com/timzowen/blog/payload/PostDto.java
+++ blogapis/src/main/java/com/timzowen/blog/payload/PostDto.java
@@ -12,5 +12,4 @@ public class PostDto {
     private String contents;
     private String description;
 
-
 }
--- blogapis/src/main/java/com/timzowen/blog/service/PostService.java
+++ blogapis/src/main/java/com/timzowen/blog/service/PostService.java
@@ -1,11 +1,16 @@
 package com.timzowen.blog.service;
 
+import com.timzowen.blog.model.Post;
 import com.timzowen.blog.payload.PostDto;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
 
 
 public interface PostService {
 
     PostDto createPost(PostDto postDto);
+
+    List<PostDto> getAllPosts();
 }
