bba74bd1492fa9bdd1926b97d15235d927891e25
TimzOwen <timzowen@gmail.com>
2024-02-25 22:50:03 +0300

    sorting in asc and desc order

--- blogapis/src/main/java/com/timzowen/blog/contollers/PostController.java
+++ blogapis/src/main/java/com/timzowen/blog/contollers/PostController.java
@@ -1,6 +1,7 @@
 package com.timzowen.blog.contollers;
 
 import com.timzowen.blog.payload.PostDto;
+import com.timzowen.blog.payload.PostResponse;
 import com.timzowen.blog.service.PostService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -23,10 +24,12 @@ public class PostController {
         return new ResponseEntity<>(postService.createPost(postDto), HttpStatus.CREATED);
     }
 
-    @GetMapping("/all-posts")
-    public List<PostDto> getAllPosts(@RequestParam(value = "pageNo",defaultValue = "",required = false) int pageNo,
-                                     @RequestParam(value = "pageSize",defaultValue = "10", required = false)int pageSize){
-       return postService.getAllPosts(pageSize,pageNo);
+    @GetMapping("/all")
+    public PostResponse getAllPosts(@RequestParam(value = "pageNo",defaultValue = "0",required = false) int pageNo,
+                                    @RequestParam(value = "pageSize",defaultValue = "10", required = false)int pageSize,
+                                    @RequestParam(value = "sortBy",defaultValue = "id",required = false) String sortBy,
+                                    @RequestParam(value = "sortDir",defaultValue = "asc",required = false) String sortDir){
+       return postService.getAllPosts(pageSize,pageNo,sortBy,sortDir);
     }
 
     @GetMapping("/{id}/post")
--- blogapis/src/main/java/com/timzowen/blog/payload/PostResponse.java
+++ blogapis/src/main/java/com/timzowen/blog/payload/PostResponse.java
@@ -2,18 +2,20 @@ package com.timzowen.blog.payload;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.List;
 
 @Getter
 @Setter
+@NoArgsConstructor
 @AllArgsConstructor
 public class PostResponse {
     private List<PostDto> content;
     private int pageNo;
     private int pageSize;
-    private int totalElements;
+    private long totalElements;
     private int totalPages;
     private boolean last;
 }
