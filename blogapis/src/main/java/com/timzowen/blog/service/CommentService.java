a0a821427667d25d991cc742e30f6b0cfc872f84
TimzOwen <timzowen@gmail.com>
2024-03-06 21:39:44 +0300

    endpoints for comments addition and fetching

--- blogapis/src/main/java/com/timzowen/blog/service/CommentService.java
+++ blogapis/src/main/java/com/timzowen/blog/service/CommentService.java
@@ -2,7 +2,11 @@ package com.timzowen.blog.service;
 
 import com.timzowen.blog.payload.CommentDto;
 
+import java.util.List;
+
 public interface CommentService {
 
     CommentDto createComment(long id, CommentDto commentDto);
+
+    List<CommentDto> getCommentsByPostId(long id);
 }
