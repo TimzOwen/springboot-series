bc27d5ed2a2ce2927fc8b1e3400de9d1552a8be6
TimzOwen <timzowen@gmail.com>
2024-03-12 23:56:42 +0300

    user

--- blogapis/src/main/java/com/timzowen/blog/model/User.java
+++ blogapis/src/main/java/com/timzowen/blog/model/User.java
@@ -25,8 +25,8 @@ public class User {
     private String password;
 
     @ManyToMany(fetch = FetchType.EAGER, cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
-    @JoinTable(name = "user_roles",
-            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
-            inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
+    @JoinTable(name = "user-roles",
+                joinColumns = @JoinColumn(name = "user_id",referencedColumnName = "id"),
+                inverseJoinColumns = @JoinColumn(name = "role_id",referencedColumnName = "id"))
     private Set<Role> roles;
 }
