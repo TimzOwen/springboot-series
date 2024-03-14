429196b90559b967e2c03682c2a7cd6a90ad6e9f
TimzOwen <timzowen@gmail.com>
2024-03-15 00:10:44 +0300

    password encoding

--- blogapis/src/main/java/com/timzowen/blog/utils/PasswordEncoderGenerator.java
+++ blogapis/src/main/java/com/timzowen/blog/utils/PasswordEncoderGenerator.java
@@ -9,6 +9,6 @@ public class PasswordEncoderGenerator {
     public static void main(String[] args) {
         PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
         System.out.println(passwordEncoder.encode("admin"));
-        System.out.println(passwordEncoder.encode("timz"));
+        System.out.println(passwordEncoder.encode("user"));
     }
 }
